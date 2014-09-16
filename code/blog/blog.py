809ab58f22a343c4549463659d0c84319d95aaf2
Tristan Louet <tristan.louet.dev@gmail.com>
2014-09-16 16:36:47 +0200

    better requests (before and teardown)

--- code/blog/blog.py
+++ code/blog/blog.py
@@ -4,6 +4,16 @@ from db import connect_db
 app = Flask(__name__)
 app.config.from_object('config')
 
+@app.before_request
+def before_request():
+    g.db = connect_db()
+
+@app.teardown_request
+def teardown_request(exception):
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
+
 @app.route('/admin')
 def adminLoggingPage():
     return render_template('adminLogging.html')
@@ -29,18 +39,15 @@ def adminLogout():
 
 @app.route('/')
 def index():
-    db = connect_db()
-    cursor = db.execute('select name, title, content, posts.id, authors.id from authors, posts where authors.id = author_id')
+    cursor = g.db.execute('select name, title, content, posts.id, authors.id from authors, posts where authors.id = author_id')
     posts = [dict(author_name=row[0], title=row[1], content=row[2], id=row[3], author_id=row[4]) for row in cursor.fetchall()]
-    db.close()
     return render_template('index.html', posts=posts)
 
 @app.route('/post/<int:post_id>')
 def getPost(post_id=None):
     if not post_id:
         abort(400)
-    db = connect_db()
-    cursor = db.execute('select name, title, content, posts.id, authors.id from authors, posts where posts.id = ? and authors.id = author_id', [post_id])
+    cursor = g.db.execute('select name, title, content, posts.id, authors.id from authors, posts where posts.id = ? and authors.id = author_id', [post_id])
     posts = [dict(author_name=row[0], title=row[1], content=row[2], id=row[3], author_id=row[4]) for row in cursor.fetchall()]
     if len(posts) == 0:
         abort(404)
@@ -50,8 +57,7 @@ def getPost(post_id=None):
 def getAuthorPosts(author_id=None):
     if not author_id:
         abort(400)
-    db = connect_db()
-    cursor = db.execute('select name, title, content, posts.id, authors.id from authors, posts where author_id = authors.id and posts.author_id = ?', [author_id])
+    cursor = g.db.execute('select name, title, content, posts.id, authors.id from authors, posts where author_id = authors.id and posts.author_id = ?', [author_id])
     posts = [dict(author_name=row[0], title=row[1], content=row[2], id=row[3], author_id=row[4]) for row in cursor.fetchall()]
     if len(posts) == 0:
         abort(404)
@@ -61,8 +67,7 @@ def getAuthorPosts(author_id=None):
 def getCategoryPosts(cat_id=None):
     if not cat_id:
         abort(400)
-    db = connect_db()
-    cursor = db.execute('select name, title, content, posts.id, authors.id from authors, posts where posts.author_id = authors.id and posts.category_id = ?', [cat_id])
+    cursor = g.db.execute('select name, title, content, posts.id, authors.id from authors, posts where posts.author_id = authors.id and posts.category_id = ?', [cat_id])
     posts = [dict(author_name=row[0], title=row[1], content=row[2], id=row[3], author_id=row[4]) for row in cursor.fetchall()]
     if len(posts) == 0:
         abort(404)
@@ -73,23 +78,20 @@ def create():
     logged = session.get('admin_logged_in', False)
     if not logged:
         return redirect(url_for('adminLoggingPage'))
-    db = connect_db()
-    cursor = db.execute('select name, id from authors')
+    cursor = g.db.execute('select name, id from authors')
     authors = [dict(name=row[0], id=row[1]) for row in cursor.fetchall()]
-    cursor = db.execute('select name, id from categories')
+    cursor = g.db.execute('select name, id from categories')
     categories = [dict(name=row[0], id=row[1]) for row in cursor.fetchall()]
-    db.close()
     return render_template('create.html', authors=authors, categories=categories)
 
 @app.route('/createPost', methods=['POST'])
 def createPost():
-    db = connect_db()
     cat_id = request.form['categoryId']
     aut_id = request.form['authorId']
     post_title = request.form['titleText']
     text = request.form['contentText']
-    db.execute('insert into posts (author_id, category_id, title, content) values (?, ?, ?, ?)', [aut_id, cat_id, post_title, text])
-    db.commit()
+    g.db.execute('insert into posts (author_id, category_id, title, content) values (?, ?, ?, ?)', [aut_id, cat_id, post_title, text])
+    g.db.commit()
     return redirect("/")
 
 if __name__ == '__main__':
